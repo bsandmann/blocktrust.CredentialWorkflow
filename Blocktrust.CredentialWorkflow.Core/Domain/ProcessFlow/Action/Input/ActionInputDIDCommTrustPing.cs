bb2312d947442b3b2c8a3dc084f1a92f0ada9bd7
ndigirigijohn <jontejonhn@gmail.com>
2024-11-25 18:02:38 +0300

    didcomm messaging and trust ping components

---
+++ Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Action/Input/ActionInputDIDCommTrustPing.cs
@@ -0,0 +1,8 @@
+﻿using Blocktrust.CredentialWorkflow.Core.Domain.Common;
+
+namespace Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Action.Input;
+
+public class ActionInputDIDCommTrustPing : ActionInput
+{
+    public ParameterReference TargetDid { get; set; } = new();
+}
---
+++ Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Action/Input/MessageFieldValue.cs
@@ -0,0 +1,10 @@
+﻿using Blocktrust.CredentialWorkflow.Core.Domain.Common;
+
+namespace Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Action.Input;
+
+public class MessageFieldValue
+{
+    public ParameterSource Source { get; set; }
+    public string Value { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+}
