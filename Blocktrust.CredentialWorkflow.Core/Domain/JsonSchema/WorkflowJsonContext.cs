520d481fe0f0b0c67fcc8734a0c1d77fee3b012a
ndigirigijohn <jontejonhn@gmail.com>
2024-12-04 15:40:32 +0300

    additional actions configurations and fixes

--- Blocktrust.CredentialWorkflow.Core/Domain/JsonSchema/WorkflowJsonContext.cs
+++ Blocktrust.CredentialWorkflow.Core/Domain/JsonSchema/WorkflowJsonContext.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Actions;
 using Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Actions.Input;
+using Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Actions.Outgoing;
 using Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Triggers;
 using Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Triggers.IncomingRequests;
 using Action = Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Actions.Action;
@@ -19,6 +20,9 @@ using ProcessFlow = ProcessFlow.ProcessFlow;
 [JsonSerializable(typeof(Action))]
 [JsonSerializable(typeof(ActionInput))]
 [JsonSerializable(typeof(MessageFieldValue))]
+[JsonSerializable(typeof(DIDCommAction))]
+[JsonSerializable(typeof(HttpAction))]
+[JsonSerializable(typeof(EmailAction))]
 public partial class WorkflowJsonContext : JsonSerializerContext
 {
 }
