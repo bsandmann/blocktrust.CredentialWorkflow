86df13af28cf77ffe5fccc2f77c7ffb547707c19
bjoern <sandmann@codedata.solutions>
2025-04-23 16:54:34 +0200

    Fixed binding issue

--- Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Actions/DID/CreateDIDAction.cs
+++ Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Actions/DID/CreateDIDAction.cs
@@ -11,11 +11,22 @@ public class CreateDIDAction : ActionInput
     [JsonPropertyName("useTenantRegistrar")]
     public bool UseTenantRegistrar { get; set; } = true;
     
+    private ParameterReference _registrarUrl;
+    private ParameterReference _walletId;
+    
     [JsonPropertyName("registrarUrl")]
-    public ParameterReference RegistrarUrl { get; set; } = new ParameterReference { Source = ParameterSource.Static };
+    public ParameterReference RegistrarUrl 
+    { 
+        get => _registrarUrl ?? new ParameterReference { Source = ParameterSource.Static }; 
+        set => _registrarUrl = value; 
+    }
     
     [JsonPropertyName("walletId")]
-    public ParameterReference WalletId { get; set; } = new ParameterReference { Source = ParameterSource.Static };
+    public ParameterReference WalletId 
+    { 
+        get => _walletId ?? new ParameterReference { Source = ParameterSource.Static }; 
+        set => _walletId = value; 
+    }
     
     [JsonPropertyName("verificationMethods")]
     public List<VerificationMethod> VerificationMethods { get; set; } = new List<VerificationMethod>();
@@ -26,42 +37,56 @@ public class CreateDIDAction : ActionInput
 
 public class VerificationMethod
 {
+    private ParameterReference _keyId;
+    private ParameterReference _purpose;
+    private ParameterReference _curve;
+    
     [JsonPropertyName("keyId")]
-    public ParameterReference KeyId { get; set; } = new ParameterReference { 
-        Source = ParameterSource.Static, 
-        DefaultValue = "key-1" 
-    };
+    public ParameterReference KeyId 
+    { 
+        get => _keyId ?? new ParameterReference { Source = ParameterSource.Static, DefaultValue = "key-1" }; 
+        set => _keyId = value; 
+    }
     
     [JsonPropertyName("purpose")]
-    public ParameterReference Purpose { get; set; } = new ParameterReference { 
-        Source = ParameterSource.Static,
-        DefaultValue = "Authentication"
-    };
+    public ParameterReference Purpose 
+    { 
+        get => _purpose ?? new ParameterReference { Source = ParameterSource.Static, DefaultValue = "Authentication" }; 
+        set => _purpose = value; 
+    }
     
     [JsonPropertyName("curve")]
-    public ParameterReference Curve { get; set; } = new ParameterReference { 
-        Source = ParameterSource.Static,
-        DefaultValue = "Secp256k1"
-    };
+    public ParameterReference Curve 
+    { 
+        get => _curve ?? new ParameterReference { Source = ParameterSource.Static, DefaultValue = "Secp256k1" }; 
+        set => _curve = value; 
+    }
 }
 
 public class ServiceEndpoint
 {
+    private ParameterReference _serviceId;
+    private ParameterReference _type;
+    private ParameterReference _endpoint;
+    
     [JsonPropertyName("serviceId")]
-    public ParameterReference ServiceId { get; set; } = new ParameterReference { 
-        Source = ParameterSource.Static,
-        DefaultValue = "service-1"
-    };
+    public ParameterReference ServiceId 
+    { 
+        get => _serviceId ?? new ParameterReference { Source = ParameterSource.Static, DefaultValue = "service-1" }; 
+        set => _serviceId = value; 
+    }
     
     [JsonPropertyName("type")]
-    public ParameterReference Type { get; set; } = new ParameterReference { 
-        Source = ParameterSource.Static,
-        DefaultValue = "LinkedDomain"
-    };
+    public ParameterReference Type 
+    { 
+        get => _type ?? new ParameterReference { Source = ParameterSource.Static, DefaultValue = "LinkedDomain" }; 
+        set => _type = value; 
+    }
     
     [JsonPropertyName("endpoint")]
-    public ParameterReference Endpoint { get; set; } = new ParameterReference { 
-        Source = ParameterSource.Static,
-        DefaultValue = ""
-    };
+    public ParameterReference Endpoint 
+    { 
+        get => _endpoint ?? new ParameterReference { Source = ParameterSource.Static, DefaultValue = "" }; 
+        set => _endpoint = value; 
+    }
 }
