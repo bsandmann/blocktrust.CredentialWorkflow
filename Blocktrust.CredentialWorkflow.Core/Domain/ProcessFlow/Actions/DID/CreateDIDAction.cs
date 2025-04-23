79fe477570bdf6234b67a49eabd79e51be340bc7
bjoern <sandmann@codedata.solutions>
2025-04-23 16:08:59 +0200

    Services section

--- Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Actions/DID/CreateDIDAction.cs
+++ Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Actions/DID/CreateDIDAction.cs
@@ -8,8 +8,20 @@ public class CreateDIDAction : ActionInput
 {
     // Id is already defined in the base class ActionInput
     
+    [JsonPropertyName("useTenantRegistrar")]
+    public bool UseTenantRegistrar { get; set; } = true;
+    
+    [JsonPropertyName("registrarUrl")]
+    public ParameterReference RegistrarUrl { get; set; } = new ParameterReference { Source = ParameterSource.Static };
+    
+    [JsonPropertyName("walletId")]
+    public ParameterReference WalletId { get; set; } = new ParameterReference { Source = ParameterSource.Static };
+    
     [JsonPropertyName("verificationMethods")]
     public List<VerificationMethod> VerificationMethods { get; set; } = new List<VerificationMethod>();
+    
+    [JsonPropertyName("services")]
+    public List<ServiceEndpoint> Services { get; set; } = new List<ServiceEndpoint>();
 }
 
 public class VerificationMethod
@@ -22,4 +34,16 @@ public class VerificationMethod
     
     [JsonPropertyName("curve")]
     public string Curve { get; set; } = "Secp256k1";
+}
+
+public class ServiceEndpoint
+{
+    [JsonPropertyName("serviceId")]
+    public string ServiceId { get; set; } = "service-1";
+    
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = "LinkedDomain";
+    
+    [JsonPropertyName("endpoint")]
+    public string Endpoint { get; set; } = "";
 }
