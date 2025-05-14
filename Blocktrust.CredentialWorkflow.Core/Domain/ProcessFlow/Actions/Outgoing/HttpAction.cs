09c07aa5ee239d0a79872e94e0bb39ea886b7cce
bjoern <sandmann@codedata.solutions>
2025-05-14 17:40:43 +0200

    WIP HttpAction

--- Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Actions/Outgoing/HttpAction.cs
+++ Blocktrust.CredentialWorkflow.Core/Domain/ProcessFlow/Actions/Outgoing/HttpAction.cs
@@ -1,7 +1,7 @@
-namespace Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Actions.Outgoing;
-
 using System.Text.Json.Serialization;
-using Common;
+using Blocktrust.CredentialWorkflow.Core.Domain.Common;
+
+namespace Blocktrust.CredentialWorkflow.Core.Domain.ProcessFlow.Actions.Outgoing;
 
 public class HttpAction : ActionInput
 {
@@ -16,4 +16,16 @@ public class HttpAction : ActionInput
     
     [JsonPropertyName("body")]
     public Dictionary<string, ParameterReference> Body { get; set; } = new();
+    
+    [JsonPropertyName("parameters")]
+    public Dictionary<string, ParameterReference> Parameters { get; set; } = new();
+    
+    public HttpAction() 
+    {
+        Id = Guid.NewGuid();
+        Endpoint = new ParameterReference { Source = ParameterSource.Static };
+        Headers = new Dictionary<string, ParameterReference>();
+        Body = new Dictionary<string, ParameterReference>();
+        Parameters = new Dictionary<string, ParameterReference>();  
+    }
 }
